626af0b3c20315fec458c89f6aa093165c52826e
vitoladev <asaptorvi@protonmail.com>
2024-05-13 06:00:55 -0300

    feat(frontend): config shadcn-ui  with packages ui-kit library into the monorepo

--- apps/ticketbom-frontend/tailwind.config.js
+++ apps/ticketbom-frontend/tailwind.config.js
@@ -1,17 +1,6 @@
-const { createGlobPatternsForDependencies } = require('@nx/react/tailwind');
-const { join } = require('path');
+const TailwindConfig = require('../../libs/ui-kit/util/src/tailwind/tailwind.config.js');
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    join(
-      __dirname,
-      '{src,pages,components,app}/**/*!(*.stories|*.spec).{ts,tsx,html}'
-    ),
-    ...createGlobPatternsForDependencies(__dirname),
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
+  ...TailwindConfig
+}
---
+++ libs/ui-kit/ui/src/index.ts
@@ -0,0 +1,7 @@
+export * from './lib/ui/button';
+export * from './lib/ui/signout-button';
+export * from './lib/ui/card';
+export * from './lib/ui/icons';
+export * from './lib/ui/input';
+export * from './lib/ui/label';
+export * from './lib/ui/separator';
