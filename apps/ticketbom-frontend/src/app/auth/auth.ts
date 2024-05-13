3815a2ccea70ab7fad133249772ab2ad740e10e6
vitoladev <asaptorvi@protonmail.com>
2024-05-13 06:02:20 -0300

    feat(auth): add authentication routes and handlers

---
+++ apps/ticketbom-frontend/src/app/auth/auth.ts
@@ -0,0 +1,61 @@
+import NextAuth from 'next-auth';
+import { users } from '@ticketbom/database';
+import GithubProvider from 'next-auth/providers/github';
+import GoogleProvider from 'next-auth/providers/google';
+import { eq } from 'drizzle-orm';
+import { db } from '../../lib/db';
+import { DrizzleAdapter } from '@auth/drizzle-adapter';
+
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  adapter: DrizzleAdapter(db),
+  session: {
+    strategy: 'jwt',
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+  pages: {
+    signIn: '/',
+    newUser: '/auth/new-user',
+    verifyRequest: '/auth/verify-request',
+  },
+  providers: [
+    GithubProvider({
+      clientId: process.env.AUTH_GITHUB_ID as string,
+      clientSecret: process.env.AUTH_GITHUB_SECRET as string,
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+  ],
+  callbacks: {
+    async session({ token, session }) {
+      if (token) {
+        session.user.id = token.id as string;
+      }
+
+      return session;
+    },
+    async jwt({ token, user }) {
+      const dbUser = await db
+        .select()
+        .from(users)
+        .where(eq(users.email, token.email || ''))
+        .limit(1)
+        .then((res) => res[0]);
+
+      if (!dbUser) {
+        if (user) {
+          token.id = user?.id;
+        }
+        return token;
+      }
+
+      return {
+        id: dbUser.id,
+        name: dbUser.name,
+        email: dbUser.email,
+        picture: dbUser.image,
+      };
+    },
+  },
+});
---
+++ apps/ticketbom-frontend/src/app/auth/login/page.tsx
@@ -0,0 +1,91 @@
+'use client';
+
+import { useState } from 'react';
+import { signIn } from 'next-auth/react';
+import {
+  Button,
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  Input,
+  Label,
+  CardTitle,
+  Icons,
+  Separator,
+} from '@ticketbom/ui-kit/ui';
+import Link from 'next/link';
+
+export const LoginPage = () => {
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleSignIn = (provider: string) => {
+    setIsLoading(true);
+
+    signIn(provider).then(() => {
+      setIsLoading(false);
+    });
+  };
+
+  return (
+    <>
+      <Card className="mx-auto max-w-[400px] space-y-6">
+        <CardHeader>
+          <CardTitle>Bem vindo ao TicketBom</CardTitle>
+          <CardDescription>Faça login para acessar o TicketBom</CardDescription>
+        </CardHeader>
+
+        <CardContent>
+          <div className="space-y-4">
+            <form>
+              <div className="space-y-2">
+                <Label htmlFor="email">Email</Label>
+                <Input
+                  id="email"
+                  placeholder="m@example.com"
+                  required
+                  type="email"
+                />
+              </div>
+              <div className="space-y-2">
+                <Label htmlFor="password">Password</Label>
+                <Input id="password" required type="password" />
+              </div>
+              <Button className="w-full" type="submit">
+                Sign In
+              </Button>
+            </form>
+          </div>
+
+          <Separator className="my-8" />
+          <div className="space-y-4">
+            <Button
+              className="w-full"
+              variant="outline"
+              onClick={() => handleSignIn('google')}
+            >
+              <Icons.google />
+              Sign in with Google
+            </Button>
+            <Button
+              className="w-full"
+              variant="outline"
+              onClick={() => handleSignIn('github')}
+            >
+              <Icons.gitHub />
+              Sign in with GitHub
+            </Button>
+            <div className="mt-4 text-center text-sm">
+              Não tem conta ainda?
+              <Link className="underline" href="#">
+                Criar
+              </Link>
+            </div>
+          </div>
+        </CardContent>
+      </Card>
+    </>
+  );
+};
+
+export default LoginPage;
