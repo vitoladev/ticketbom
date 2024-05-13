feedda6ad850c3e4531399bf821959de1459617a
vitoladev <asaptorvi@protonmail.com>
2024-05-13 06:03:03 -0300

    feat(ui): add input, label, and separator and SVG icons UI components

---
+++ libs/ui-kit/ui/src/lib/ui/signout-button.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import { signOut } from 'next-auth/react';
+
+export const SignOutButton = () => {
+  return (
+    <button onClick={() => signOut()}>
+      Sign out
+    </button>
+  );
+};
