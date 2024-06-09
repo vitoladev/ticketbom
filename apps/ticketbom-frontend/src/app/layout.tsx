b551af9f9863550ae629c8d7da10fa3d1aa3061c
vitoladev <asaptorvi@protonmail.com>
2024-06-09 05:30:12 -0300

    refactor: Add navigation bar to RootLayout component

--- apps/ticketbom-frontend/src/app/layout.tsx
+++ apps/ticketbom-frontend/src/app/layout.tsx
@@ -3,19 +3,29 @@ import Footer from '../components/ui/footer';
 import React from 'react';
 import { ReactQueryProvider } from '../providers/ReactQueryProvider';
 import { OrderProvider } from '../providers/OrderProvider';
+import Nav from '../components/ui/nav';
+import { auth } from './auth/auth';
 
 export const metadata = {
   title: 'TicketBom',
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const session = await auth();
+
   return (
     <html lang="en">
       <body>
+        <Nav
+          user={{
+            name: session?.user?.name || '',
+            image: session?.user?.image || '',
+          }}
+        />
         <ReactQueryProvider>
           <OrderProvider>{children}</OrderProvider>
         </ReactQueryProvider>
