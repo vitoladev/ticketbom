48879a235680d089229e430155d736da42f1dddf
vitoladev <asaptorvi@protonmail.com>
2024-06-09 07:12:47 -0300

    refactor: add Cognito authentication provider in NextAuth configuration

--- apps/ticketbom-frontend/src/app/auth/auth.ts
+++ apps/ticketbom-frontend/src/app/auth/auth.ts
@@ -1,13 +1,12 @@
 import NextAuth from 'next-auth';
-import { users } from '@ticketbom/database';
-import GithubProvider from 'next-auth/providers/github';
-import GoogleProvider from 'next-auth/providers/google';
-import { eq } from 'drizzle-orm';
-import { db } from '../../lib/db';
-import { DrizzleAdapter } from '@auth/drizzle-adapter';
+// import { users } from '@ticketbom/database';
+import CognitoProvider from 'next-auth/providers/cognito';
+// import { DrizzleAdapter } from '@auth/drizzle-adapter';
+// import { eq } from 'drizzle-orm';
+// import { db } from '../../lib/db';
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  adapter: DrizzleAdapter(db),
+  // adapter: DrizzleAdapter(db),
   session: {
     strategy: 'jwt',
   },
@@ -19,13 +18,15 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
     verifyRequest: '/auth/verify-request',
   },
   providers: [
-    GithubProvider({
-      clientId: process.env.AUTH_GITHUB_ID as string,
-      clientSecret: process.env.AUTH_GITHUB_SECRET as string,
-    }),
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    CognitoProvider({
+      clientId: process.env.COGNITO_CLIENT_ID,
+      clientSecret: process.env.COGNITO_CLIENT_SECRET,
+      issuer: process.env.COGNITO_ISSUER,
+      authorization: {
+        params: {
+          scope: 'openid profile email',
+        },
+      },
     }),
   ],
   callbacks: {
@@ -35,28 +36,6 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
       }
 
       return session;
-    },
-    async jwt({ token, user }) {
-      const dbUser = await db
-        .select()
-        .from(users)
-        .where(eq(users.email, token.email || ''))
-        .limit(1)
-        .then((res) => res[0]);
-
-      if (!dbUser) {
-        if (user) {
-          token.id = user?.id;
-        }
-        return token;
-      }
-
-      return {
-        id: dbUser.id,
-        name: dbUser.name,
-        email: dbUser.email,
-        picture: dbUser.image,
-      };
     },
   },
 });
