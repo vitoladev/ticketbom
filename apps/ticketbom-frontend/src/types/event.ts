f51231fe2b2094f8b446a5f4c115c7ba59a70e28
vitoladev <asaptorvi@protonmail.com>
2024-06-08 21:55:37 -0300

    feat: Add Event, Ticket, and EventType types

---
+++ apps/ticketbom-frontend/src/types/event.ts
@@ -0,0 +1,8 @@
+export type EventType = {
+  id: string;
+  title: string;
+  description: string;
+  date: string;
+  location: string;
+  status: string;
+}
---
+++ apps/ticketbom-frontend/src/types/ticket.ts
@@ -0,0 +1,5 @@
+export type TicketType = {
+  id: string;
+  price: number;
+  status: string;
+}
