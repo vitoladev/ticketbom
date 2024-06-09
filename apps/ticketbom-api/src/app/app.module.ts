a8101b9c33174b8dfb5194b395aeb5c53a2da81d
vitoladev <asaptorvi@protonmail.com>
2024-06-08 21:54:52 -0300

    feat: Add EventsModule and TicketsModule to app.module.ts

--- apps/ticketbom-api/src/app/app.module.ts
+++ apps/ticketbom-api/src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppService } from './app.service';
 import { DrizzlePGModule } from '../common/drizzle/drizzle.module';
 import * as schema from '@ticketbom/database';
 import { UsersModule } from '../users/users.module';
+import { EventsModule } from '../events/events.module';
+import { TicketsModule } from '../tickets/tickets.module';
 
 @Module({
   imports: [
@@ -23,6 +25,8 @@ import { UsersModule } from '../users/users.module';
       config: { schema: { ...schema } },
     }),
     UsersModule,
+    EventsModule,
+    TicketsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
