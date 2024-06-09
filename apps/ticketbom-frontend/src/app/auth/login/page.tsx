5ec5ed79c719697b1651a1b345ab405f9a6ec8b9
vitoladev <asaptorvi@protonmail.com>
2024-06-08 21:58:24 -0300

    chore: Update login page description

--- apps/ticketbom-frontend/src/app/auth/login/page.tsx
+++ apps/ticketbom-frontend/src/app/auth/login/page.tsx
@@ -8,15 +8,13 @@ import {
   CardContent,
   CardDescription,
   CardHeader,
-  Input,
-  Label,
   CardTitle,
   Icons,
   Separator,
 } from '@ticketbom/ui-kit/ui';
 import Link from 'next/link';
 
-export const LoginPage = () => {
+const LoginPage = () => {
   const [isLoading, setIsLoading] = useState(false);
 
   const handleSignIn = (provider: string) => {
@@ -31,31 +29,10 @@ export const LoginPage = () => {
     <Card className="mx-auto max-w-[400px] space-y-6">
       <CardHeader>
         <CardTitle>Bem vindo ao TicketBom</CardTitle>
-        <CardDescription>Faça login para acessar o TicketBom</CardDescription>
+        <CardDescription>Faça login para acessar a plataforma</CardDescription>
       </CardHeader>
 
       <CardContent>
-        <div className="space-y-4">
-          <form>
-            <div className="space-y-2">
-              <Label htmlFor="email">Email</Label>
-              <Input
-                id="email"
-                placeholder="m@example.com"
-                required
-                type="email"
-              />
-            </div>
-            <div className="space-y-2">
-              <Label htmlFor="password">Password</Label>
-              <Input id="password" required type="password" />
-            </div>
-            <Button className="w-full" type="submit">
-              Sign In
-            </Button>
-          </form>
-        </div>
-
         <Separator className="my-8" />
         <div className="space-y-4">
           <Button
@@ -76,7 +53,7 @@ export const LoginPage = () => {
           </Button>
           <div className="mt-4 text-center text-sm">
             Não tem conta ainda?
-            <Link className="underline" href="#">
+            <Link className="underline" href="/auth/signup">
               Criar
             </Link>
           </div>
