681ac3193aa6767de24238edbf6800336a6ce937
vitoladev <asaptorvi@protonmail.com>
2024-06-09 05:30:28 -0300

    refactor: Update navigation bar link text to include domain name

--- apps/ticketbom-frontend/src/components/ui/nav.tsx
+++ apps/ticketbom-frontend/src/components/ui/nav.tsx
@@ -17,7 +17,7 @@ export default function Nav({
   return (
     <header className="flex h-20 w-full shrink-0 items-center px-4 md:px-6">
       <Link className="mr-6 hidden lg:flex" href="#">
-        <span className="sr-only">TicketBom</span>
+        <span className="sr-only">TicketBom.live</span>
       </Link>
       <NavigationMenu className="lg:flex">
         <NavigationMenuList>
@@ -36,14 +36,6 @@ export default function Nav({
               href="/events"
             >
               Events
-            </Link>
-          </NavigationMenuLink>
-          <NavigationMenuLink asChild>
-            <Link
-              className="group inline-flex h-9 w-max items-center justify-center rounded-md bg-white px-4 py-2 text-sm font-medium transition-colors hover:bg-gray-100 hover:text-gray-900 focus:bg-gray-100 focus:text-gray-900 focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-gray-100/50 data-[state=open]:bg-gray-100/50 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus:bg-gray-800 dark:focus:text-gray-50 dark:data-[active]:bg-gray-800/50 dark:data-[state=open]:bg-gray-800/50"
-              href="/about"
-            >
-              About
             </Link>
           </NavigationMenuLink>
 
