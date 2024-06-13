f957b76982da011b69b68dd4dc99b31be0b89280
vitoladev <asaptorvi@protonmail.com>
2024-06-12 21:24:14 -0300

    chore: Remove commented out code and unused endpoints in TicketsController

--- apps/ticketbom-api/src/tickets/tickets.controller.ts
+++ apps/ticketbom-api/src/tickets/tickets.controller.ts
@@ -17,25 +17,6 @@ export class TicketsController {
     return this.ticketsService.findByEventId(eventId);
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.ticketsService.findOne(id);
-  }
-
-  @Post(':id/start-order')
-  startOrder(@Param('id') id: string) {
-    return this.ticketsService.startOrder({ ticketId: id, userId: '123' });
-  }
-
-  // @Get('verify/:id')
-  // verify(@Param('id') id: string) {
-  //   return this.ticketsService.verifyIfTicketIsAvailable(id);
-  // }
-
-  // @Patch(':id')
-  // update(@Param('id') id: string, @Body() updateTicketDto: ReserveTicketDto) {
-  //   return this.ticketsService.update(id, updateTicketDto);
-  // }
 
   @Delete(':id')
   remove(@Param('id') id: string) {
