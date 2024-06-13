02cb604ab3d8198b2565f588a01c1950257b24c5
vitoladev <asaptorvi@protonmail.com>
2024-06-12 21:26:09 -0300

    feat: Add TicketEntity with status enum and properties

--- apps/ticketbom-api/src/tickets/entities/ticket.entity.ts
+++ apps/ticketbom-api/src/tickets/entities/ticket.entity.ts
@@ -1,1 +1,15 @@
-export class Ticket {}
+export enum TicketStatus {
+  AVAILABLE = 'AVAILABLE',
+  SOLD_OUT = 'SOLD_OUT',
+  CANCELLED = 'CANCELLED',
+}
+
+
+export class TicketEntity {
+  id: string;
+  title: string;
+  eventId: string;
+  price: number;
+  quantityTotal: number;
+  status: 'AVAILABLE' | 'SOLD_OUT' | 'CANCELLED';
+}
