0f5db43110cee8302652cc02b4ec9b0c60a99255
vitoladev <asaptorvi@protonmail.com>
2024-06-13 22:34:46 -0300

    chore: Update .gitignore to include .env.local file and fix formatting in main.ts

--- apps/ticketbom-api/src/main.ts
+++ apps/ticketbom-api/src/main.ts
@@ -35,7 +35,7 @@ async function bootstrap() {
 
   const port = process.env.PORT || 3000;
 
-  await app.listen(port);
+  await app.listen(port, '0.0.0.0');
   Logger.log(
     `🚀 Application is running on: ${await app.getUrl()}/${globalPrefix}`
   );
