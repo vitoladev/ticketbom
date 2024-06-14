66181ca9bec5e72d47fcc30323d63471f9ea93c3
vitoladev <asaptorvi@protonmail.com>
2024-06-14 06:23:06 -0300

    refactor: Handle duplicate key value violation in HttpExceptionFilter

--- apps/ticketbom-api/src/common/filters/http-exception.filter.ts
+++ apps/ticketbom-api/src/common/filters/http-exception.filter.ts
@@ -5,25 +5,50 @@ import {
   HttpException,
 } from '@nestjs/common';
 import { FastifyReply, FastifyRequest } from 'fastify';
+import { EventAlreadyExistsException } from '../../events/events.exceptions';
 
 @Catch()
 export class HttpExceptionFilter implements ExceptionFilter {
-  catch(exception: HttpException, host: ArgumentsHost) {
+  catch(exception: unknown, host: ArgumentsHost) {
     const ctx = host.switchToHttp();
     const response = ctx.getResponse<FastifyReply>();
     const request = ctx.getRequest<FastifyRequest>();
-    const status = exception.getStatus();
+    if (exception instanceof HttpException) {
+      const status = exception.getStatus();
 
-    const isInternal = status === 500;
-    if (isInternal) {
-      request.log.error(exception);
+      const isInternal = status === 500;
+      if (isInternal) {
+        console.log(exception);
+      }
+
+      const message = isInternal ? 'Internal Server Error' : exception.message;
+
+      response.status(status).send({
+        statusCode: status,
+        message,
+        timestamp: new Date().toISOString(),
+      });
     }
 
-    const message = isInternal ? 'Internal Server Error' : exception.message;
+    if (
+      exception instanceof Error &&
+      exception.message.includes(
+        'duplicate key value violates unique constraint'
+      )
+    ) {
+      const isEvent = request.url.includes('events');
+      if (isEvent) throw new EventAlreadyExistsException(exception);
 
-    response.status(status).send({
-      statusCode: status,
-      message,
+      response.status(409).send({
+        statusCode: 409,
+        message: 'Conflict',
+        timestamp: new Date().toISOString(),
+      });
+    }
+
+    response.status(500).send({
+      statusCode: 500,
+      message: 'Internal Server Error',
       timestamp: new Date().toISOString(),
     });
   }
