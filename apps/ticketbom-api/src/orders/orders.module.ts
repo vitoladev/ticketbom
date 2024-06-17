e8337e235e7e4a8c8ebd3fc5ad88c834b8920b0f
vitoladev <asaptorvi@protonmail.com>
2024-06-17 02:43:55 -0300

    refactor(orders): Update OrdersModule to include TicketsService and repositories

--- apps/ticketbom-api/src/orders/orders.module.ts
+++ apps/ticketbom-api/src/orders/orders.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { OrdersService } from './orders.service';
 import { OrdersController } from './orders.controller';
+import { OrdersRepositoryProvider } from './orders.repository';
+import { TicketsRepositoryProvider } from '../tickets/tickets.repository';
+import { TicketsService } from '../tickets/tickets.service';
 
 @Module({
   controllers: [OrdersController],
-  providers: [OrdersService],
+  providers: [
+    TicketsRepositoryProvider,
+    TicketsService,
+    OrdersRepositoryProvider,
+    OrdersService,
+  ],
 })
 export class OrdersModule {}
