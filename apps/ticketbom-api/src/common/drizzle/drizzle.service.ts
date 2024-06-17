a3a01090c7fdad882842f9542a043b751505cd85
vitoladev <asaptorvi@protonmail.com>
2024-06-17 20:14:00 -0300

    refactor(drizzle): Update DrizzlePGService to handle database shutdown

--- apps/ticketbom-api/src/common/drizzle/drizzle.service.ts
+++ apps/ticketbom-api/src/common/drizzle/drizzle.service.ts
@@ -1,22 +1,42 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger, OnApplicationShutdown } from '@nestjs/common';
 import { drizzle } from 'drizzle-orm/node-postgres';
 import { Client, Pool } from 'pg';
+import { tables } from '@ticketbom/database';
 import { DrizzlePGConfig } from './drizzle.interface';
 import { ConfigService } from '@nestjs/config';
 
 @Injectable()
-export class DrizzlePGService {
+export class DrizzlePGService implements OnApplicationShutdown {
   constructor(private readonly configService: ConfigService) {}
+  databaseInstance?: Client | Pool;
+
+  onApplicationShutdown(signal?: string) {
+    console.log({ instance: this.databaseInstance, signal })
+    Logger.log('Shutting down drizzle: ' + signal);
+    if (signal === 'SIGINT' || signal === 'SIGTERM') {
+      this.databaseInstance?.end((err) => {
+        if (err) {
+          Logger.error('Error shutting down drizzle: ' + err);
+        }
+        Logger.log('Drizzle shut down');
+      });
+    }
+  }
 
   public async getDrizzle(options: DrizzlePGConfig) {
     const config = this.configService.get('database');
 
     if (options.connection === 'client') {
-      const client = new Client(config);
-      await client.connect();
-      return drizzle(client, options?.config);
+      this.databaseInstance = new Client(config);
+      await this.databaseInstance.connect();
+      return drizzle(this.databaseInstance, {
+        schema: { ...tables },
+      });
     }
-    const pool = new Pool(config);
-    return drizzle(pool, options?.config);
+
+    this.databaseInstance = new Pool(config);
+    return drizzle(this.databaseInstance, {
+      schema: { ...tables },
+    });
   }
 }
