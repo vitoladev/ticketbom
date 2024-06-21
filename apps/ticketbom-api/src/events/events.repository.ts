f427464d9f136d28908eb447a7cdd15b01493aef
vitoladev <asaptorvi@protonmail.com>
2024-06-21 05:30:49 -0300

    refactor(events): Update EventsRepository to include tickets in findOneWithTickets

--- apps/ticketbom-api/src/events/events.repository.ts
+++ apps/ticketbom-api/src/events/events.repository.ts
@@ -6,6 +6,7 @@ import {
   IRepository,
   DrizzleDatabase,
   TicketEntity,
+  tables,
 } from '@ticketbom/database';
 import { eq } from 'drizzle-orm';
 
@@ -24,15 +25,58 @@ class EventsRepository extends BaseRepository<typeof events._.config> {
     super(db, 'events');
   }
 
-  async findOneWithTickets(
-    id: string
-  ): Promise<EventEntity & { tickets: TicketEntity[] }> {
-    return this.db.query.events.findFirst({
-      where: eq(events.id, id),
-      with: {
-        tickets: true,
-      },
-    });
+  async findOneWithTickets(id: string): Promise<
+    | (Pick<
+        EventEntity,
+        'id' | 'title' | 'description' | 'status' | 'location'
+      > & {
+        tickets: Pick<TicketEntity, 'id' | 'title' | 'price' | 'status'>[];
+      })
+    | null
+  > {
+    const rows = await this.db
+      .select({
+        id: events.id,
+        title: events.title,
+        description: events.description,
+        date: events.date,
+        location: events.location,
+        status: events.status,
+        tickets: {
+          id: tables.tickets.id,
+          title: tables.tickets.title,
+          eventId: tables.tickets.eventId,
+          price: tables.tickets.price,
+          status: tables.tickets.status,
+        },
+      })
+      .from(events)
+      .where(eq(events.id, id))
+      .fullJoin(tables.tickets, eq(tables.tickets.eventId, events.id));
+
+    if (rows.length === 0) {
+      return null;
+    }
+
+    const event = {
+      id: rows[0].id,
+      title: rows[0].title,
+      description: rows[0].description,
+      date: rows[0].date,
+      location: rows[0].location,
+      status: rows[0].status,
+      tickets:
+        rows[0]?.tickets?.id !== undefined
+          ? rows.map((row) => ({
+              id: row.tickets.id,
+              title: row.tickets.title,
+              price: row.tickets.price,
+              status: row.tickets.status,
+            }))
+          : [],
+    };
+
+    return event;
   }
 
   async findManyByOrganizerId(organizerId: string): Promise<EventEntity[]> {
